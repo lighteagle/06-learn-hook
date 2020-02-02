93f57260f888fab26e6bb8be8a3359e6ac161d44
lighteagle <sofyan.agus18@gmail.com>
2020-02-02 20:26:37 +0700

    update useContext with useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import "./App.css";
-import CounterThree from "./components/useReducer/CounterThree";
 
 function App() {
-  return (
-    <div className="App">
-      <CounterThree />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
